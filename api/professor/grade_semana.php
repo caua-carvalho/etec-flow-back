92c92f268af3fab8664e1d9252847ae32d7cd4d2
caua-carvalho <caua.carma033@gmail.com>
2025-06-18 10:44:32 -0300

    caminho para arquivo de conn bd

--- api/professor/grade_semana.php
+++ api/professor/grade_semana.php
@@ -6,7 +6,7 @@ header('Content-Type: application/json; charset=UTF-8');
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-require_once __DIR__ . '/../config/conn.php';
+require_once __DIR__ . '/../../config/conn.php';
 
 // pega parâmetros
 $professor_id = isset($_GET['professor_id']) ? (int) $_GET['professor_id'] : 0;
